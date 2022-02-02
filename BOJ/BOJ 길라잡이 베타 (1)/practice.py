678a1eedf32aa1ee30a3c68747bea19f3617e1ba
moo <mu3980@gmail.com>
2022-02-02 23:34:34 +0900

    coommit on  20220202

--- BOJ/BOJ 길라잡이 베타 (1)/practice.py
+++ BOJ/BOJ 길라잡이 베타 (1)/practice.py
@@ -1,18 +1,31 @@
-N=int(input())
-table=[[0 for _ in range(N+2)] for _ in range(N+2)]
-
-inputTable=[]
-for _ in range(N):
-    inputTable.append(list(map(int,input())))
-
-for i in range(N):
-    for j in range(N):
-        table[i+1][j+1] = inputTable[i][j]
-def sumlist(arr):
-    cnt=0
-    for i in arr:
-        for j in i:
-            cnt+=j
-    return cnt
-
-print(sumlist(table))
+from collections import deque
+import sys
+T=int(sys.stdin.readline())
+for _ in range(T):
+    command=list(map(str,sys.stdin.readline().rstrip()))
+    n=int(sys.stdin.readline())
+    ls=str(sys.stdin.readline().rstrip())
+    if ls == "[]":
+        ls=deque([])
+    else:
+        ls=deque(list(map(str,ls[1:len(ls)-1].split(","))))
+    sw=True
+    r_c=0
+    for i in command:
+        if i == "R":
+            r_c += 1
+        elif i == "D":
+            if ls:
+                if r_c % 2 == 0:
+                    ls.popleft()
+                else:
+                    ls.pop()
+            else:
+                print("error")
+                sw=False
+                break
+    if r_c % 2 == 1:
+        ls.reverse()
+    answer=""
+    if sw:
+        print("["+",".join(ls)+"]")
