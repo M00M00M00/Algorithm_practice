af43e507547a4ccd5c224630d9b717c6b9a9119e
moo <mu3980@gmail.com>
2022-03-16 23:36:17 +0900

    commit on 16/03/2022

--- BOJ/sloved.ac_essential/class 2/18111.py
+++ BOJ/sloved.ac_essential/class 2/18111.py
@@ -1,5 +1,27 @@
+import sys
+
 N,M,B = map(int,input().split())
 arr = []
 for _ in range(N):
-    arr.append(list(map(int,input().split())))
-start = 0
+    arr.append(list(map(int,sys.stdin.readline().split())))
+ans = 500 * 500 * 256 * 2
+anss = 0
+e = 257
+for i in range(e):
+    mmax = 0
+    mmin = 0
+    for j in range(len(arr)):
+        for k in range(len(arr[0])):
+            if arr[j][k] < i:
+                mmin += i - arr[j][k]
+            else:
+                mmax += (arr[j][k] - i)
+    inven = mmax + B - mmin
+    if inven < 0:
+        continue
+    time = 2 * mmax + mmin
+    if time <= ans:
+        ans = time
+        anss = i
+    
+print(ans, anss)
