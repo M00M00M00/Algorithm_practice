f8a35047305ed49e9341b2421a85a17ae8167cc3
moo <mu3980@gmail.com>
2022-04-10 20:13:42 +0900

    commit on 10/04/2022

--- BOJ/sloved.ac_essential/class 3/1074.py
+++ BOJ/sloved.ac_essential/class 3/1074.py
@@ -1,5 +1,25 @@
 N, r, c = map(int, input().split())
-r, c += 1
-ans = 0
-while (max(r, c)) < 4:
-    
+start = 0
+end = 4 ** N - 1
+while True:
+    quar = (end - start + 1) // 4
+    pivot = (2 ** N) // 2
+    if r < pivot and c < pivot:
+        end -= 3 * quar
+    elif (r < pivot and c >= pivot):
+        c -= pivot
+        start += quar
+        end -= 2 * quar
+    elif (r >= pivot and c < pivot):
+        r -= pivot
+        start += 2 * quar
+        end -= quar
+    else:
+        r -= pivot
+        c -= pivot
+        start += 3 * quar
+    if N == 0:
+        break
+    N -= 1
+
+print(start)
--- BOJ/코딩 테스트 준비 - 기초 - 브루트 포스/practice.py
+++ BOJ/코딩 테스트 준비 - 기초 - 브루트 포스/practice.py
@@ -4,3 +4,4 @@ for _ in range(N):
     arr.append(list(map(int,input().split())))
     
 print(arr[1:3][0:4])
+
