bf3abad1b873b9d1500f90f6fffe6c650f0a98e7
moo <mu3980@gmail.com>
2022-04-30 00:03:49 +0900

    commit on 30/04/2022

--- BOJ/sloved.ac_essential/class 3/5525.py
+++ BOJ/sloved.ac_essential/class 3/5525.py
@@ -7,15 +7,15 @@ s = str(input())
 pi = [0] +list(range(2 * N))
 i = 0
 cnt = 0
-while (i + 2 * N + 1) < l:
-    j = 0
-    while (j < 2 * N + 1):
-        if find_s[j] != s[i + j]:
-            break;
-        j += 1
-    if j == 2 * N + 1:
-        cnt += 1
-        i += 2
-    else:
-        i += pi[j] + 1
+j = 0
+while (i) < l:
+    while j != 0 and s[i] != find_s[j]:
+        j = pi[j - 1]
+    if s[i] == find_s[j]:
+        if (j == 2 * N):
+            cnt += 1
+            j = pi[j]
+        else:
+            j += 1
+    i += 1 
 print(cnt)
